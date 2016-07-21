bdfc0fde3a888c8973f0a216535111d272d2b4eb
Gleicon Moraes <gleicon@gmail.com>
2016-07-21 12:21:39 -0300

    birl

--- main.go
+++ main.go
@@ -13,13 +13,13 @@ import (
 func main() {
 	redisServer := flag.String("r", "localhost:6379", "Redis server addr:port")
 	flag.Usage = func() {
-		fmt.Println("Usage: uurl -r localhost:6379 ")
+		fmt.Println("Usage: uurl -r localhost:6379/?db=2 ")
 		os.Exit(1)
 	}
 	flag.Parse()
 	db := redis.New(*redisServer)
 	uu := NewUURL(db)
 	hr := NewHTTPResources(uu)
-	http.Handle("/", hr.serverMux)
+	http.Handle("/", HTTPLogger(hr.serverMux))
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
